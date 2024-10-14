d6775654a24d1f2762ccffdc5f092a20b5fd0551
or <enockdeghost@gmal.com>
2024-10-14 10:32:52 -0700

    copyAll

--- copyAll.py
+++ copyAll.py
@@ -1,6 +1,8 @@
+# import from python libraries
 import os 
 import shutil 
 
+# creat a fuction for source and destination of files
 def copy_files_to_usb(source_folder, destination_folder):
      if not os.path.exists(source_folder): 
         print(f"Source folder {source_folder} does not exist.") 
@@ -8,10 +10,12 @@ def copy_files_to_usb(source_folder, destination_folder):
      if not os.path.exists(destination_folder): 
         print(f"Destination folder {destination_folder} does not exist.") 
         return 
+        # create a loop for get those data one by one
      for item in os.listdir(source_folder): 
         source_item = os.path.join(source_folder, item)
         destination_item = os.path.join(destination_folder, item) 
         try: 
+            # if path is exits
             if os.path.isfile(source_item):
                 print(f"Copying file: {source_item}") 
                 shutil.copy2(source_item, destination_item) 
@@ -20,7 +24,9 @@ def copy_files_to_usb(source_folder, destination_folder):
                 shutil.copytree(source_item, destination_item) 
         except Exception as e: print(f"Error copying {source_item}: {e}")
 if __name__ == "__main__": 
+    # desktop path
     desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+    # lebel of Letter of you drive
     usb_drive_path = 'G:\\' 
     copy_files_to_usb(desktop_path, usb_drive_path) 
 
